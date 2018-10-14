f3813de71d16f98b354e110cca58fca49f43d42c
Tushar <tusharnimje21@gmail.com>
2018-10-14 23:04:54 +0530

    jest setup

---
+++ src/common/table/tableComponent.spec.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Table from "./tableComponent";
+
+describe("Table", () => {
+  it("should render correctly", () => {
+    const component = shallow(<Table />);
+  });
+
+  it("should render initial layout", () => {
+    const component = shallow(<Table />);
+    expect(component.getElements()).toMatchSnapshot();
+  });
+});
